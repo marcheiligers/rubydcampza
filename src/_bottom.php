d643e30b93b1fe21ef3b47451c1a71918a96ed99
Sascha Michael Trinkaus <saschamt@me.com>
2014-07-23 16:46:13 +0200

    adds src and first version of the design

---
+++ src/_bottom.php
@@ -0,0 +1,17 @@
+<section class='contact'>
+  <div class='contact__mapbox'>
+    <div id='map-canvas'></div>
+  </div>
+  <div class='contact__wrapper' id='contact'>
+    <h1 data-translate='contact__h1'></h1>
+    <div class='info'>
+      <p>
+        Email:
+        <a href='mailto:info@EMAILDOMAIN'>
+        </a>
+      </p>
+    </div>
+  </div>
+</section>
+<footer>© <?php date_default_timezone_set('Europe/Berlin'); echo date('Y'); ?> <strong>RubyDCampZA</strong><br/></a></footer>
+<script src='javascript/rdcza.min.js'></script>
---
+++ src/rdcza.js
@@ -0,0 +1,27 @@
+document.documentElement.setAttribute('data-ua', navigator.userAgent);
+
+$(document).ready(function() {
+
+  Modernizr.addTest('backgroundcliptext', function() {
+    var div = document.createElement('div');
+    div.style.webkitBackgroundClip = "text";
+    var text = div.style.cssText.indexOf('text');
+    if (text > 0) { return true; }
+    'Webkit Moz O ms Khtml'.replace(/([A-Za-z]*)/g, function(val) {
+      if (val + 'BackgroundClip' in div.style) { return true; }
+    });
+  });
+
+  // Google Maps API v3
+  function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(-25.446876,30.884290),
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+});
